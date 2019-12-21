b7630db6be86e1a5310e07b952eef0df17b12ffc
ActualMrinja <42427718+ActualMrinja@users.noreply.github.com>
2019-12-20 21:58:56 -0600

    Add files via upload

--- JS Files/GameData.js
+++ JS Files/GameData.js
@@ -594,15 +594,7 @@ maingame = function() {
     textmaker("SCORE: " + (endgame ? "000" : score), 20, 33.5, 20);
 
     //Ipads cannot go full screen
-    if(((window.innerWidth && document.documentElement.clientWidth) ?
-            Math.min(window.innerWidth, document.documentElement.clientWidth) :
-            window.innerWidth ||
-            document.documentElement.clientWidth ||
-            document.body.clientWidth)/((window.innerHeight && document.documentElement.clientHeight) ?
-            Math.min(window.innerHeight, document.documentElement.clientHeight) :
-            window.innerHeight ||
-            document.documentElement.clientHeight ||
-            document.body.clientWidth) !== 0.75){
+    if(((outerWidth)/(outerHeight)).toFixed(2) !== "0.75"){
      (collision(mousex, mousey, 0, 0, (hs / 297) * 485, (hs / 297) * 8, (hs / 297) * 30, (hs / 297) * 30)) ? ctx.globalAlpha = 1:
      ctx.globalAlpha = 0.85;
      ctx.drawImage((ws == 528) ? gifload[1] : gifload[2], (hs / 297) * 485, (hs / 297) * 8, (hs / 297) * 30, (hs / 297) * 30);
